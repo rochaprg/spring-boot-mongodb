b3a5240f439763f2dcdb1e36e5176901ed4ede54
rochaprg <rochaprg@gmail.com>
2021-09-09 10:26:01 -0300

    Criando post com usuario aninhado

--- workshopmongo/src/main/java/com/paulorocha/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/paulorocha/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.paulorocha.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.paulorocha.workshopmongo.domain.Post;
 import com.paulorocha.workshopmongo.domain.User;
+import com.paulorocha.workshopmongo.repository.PostRepository;
 import com.paulorocha.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +19,26 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post post1 = new Post(null, sdf.parse("23/05/2019"), "Partiu viagem", "Vou viajar para praia !!!",maria);
+		Post post2 = new Post(null, sdf.parse("13/05/2020"), "Partiu disney", "Vou viajar para praia !!!",maria);
+		
+		postRepository.save(post1);
 	}
 
 }
---
+++ workshopmongo/src/main/java/com/paulorocha/workshopmongo/domain/Post.java
@@ -0,0 +1,75 @@
+package com.paulorocha.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Post implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+	private Date data;
+	private String title;
+	private String body;
+	
+	private User author;
+	
+	public Post () {
+		
+	}
+
+	public Post(String id, Date data, String title, String body,User author) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.title = title;
+		this.body = body;
+		this.author = author;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	
+	
+}
